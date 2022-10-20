5b884e00da20cbee35eb63aa5d3ace4fc345a74e
unknown <adem.daami@esprit.tn>
2022-10-20 21:41:08 +0100

    docker produit service

--- src/main/java/com/ecommerce/microservice/EcommerceProductApplication.java
+++ src/main/java/com/ecommerce/microservice/EcommerceProductApplication.java
@@ -5,13 +5,14 @@ import java.util.stream.Stream;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import com.ecommerce.microservice.entity.Product;
 import com.ecommerce.microservice.repository.ProductRepository;
 
 
-
+@EnableEurekaClient
 @SpringBootApplication
 public class EcommerceProductApplication {
 
